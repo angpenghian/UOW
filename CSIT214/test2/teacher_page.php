ae6022857c7a4dd74207829fefd2ff58ac620601
PengHian Ang <v_fphhang@tencent.com>
2023-10-10 18:59:24 +0800

    update

--- CSIT214/test2/teacher_page.php
+++ CSIT214/test2/teacher_page.php
@@ -25,111 +25,96 @@
             </form>
         </header>
 
-        <div class="background">
-            <div class="container2">
-                <h2>Create Room Booking</h2>
+        <div class="background"></div>
+        <div class="container2">
+            <h2>Create Room Booking</h2>
 
-                <form id="booking-form">
-                    <label for="room_name">Room Name:</label><br>
-                    <input type="text" id="room_name" name="room_name" required><br>
+            <form id="booking-form">
+                <label for="room_name">Room Name:</label><br>
+                <input type="text" id="room_name" name="room_name" required><br>
 
-                    <label for="room_type">Room Type:</label><br>
-                    <input type="text" id="room_type" name="room_type" required><br>
+                <label for="room_type">Room Type:</label><br>
+                <input type="text" id="room_type" name="room_type" required><br>
 
-                    <label for="room_capacity">Room Capacity:</label><br>
-                    <input type="text" id="room_capacity" name="room_capacity" required><br>
+                <label for="room_capacity">Room Capacity:</label><br>
+                <input type="text" id="room_capacity" name="room_capacity" required><br>
 
-                    <label for="room_price">Room Price:</label><br>
-                    <input type="text" id="room_price" name="room_price" required><br>
+                <label for="room_price">Room Price:</label><br>
+                <input type="text" id="room_price" name="room_price" required><br>
 
-                    <label for="date">Date:</label><br>
-                    <input type="date" id="date" name="date" required><br>
+                <label for="date">Date:</label><br>
+                <input type="date" id="date" name="date" required><br>
 
-                    <button type="button" onclick="createBooking()">Create Booking</button>
-                </form>
+                <button type="button" onclick="createBooking()">Create Booking</button>
+            </form>
 
-                <h2>Available Bookings</h2> <!-- Header for Available Bookings -->
+            <br/>
+            <h2>Available Bookings</h2> <!-- Header for Available Bookings -->
 
-                <div id="available-bookings"></div>
+            <div id="available-bookings"></div>
 
-                <script>
-                    function createBooking() {
-                        const roomName = document.getElementById('room_name').value;
-                        const roomType = document.getElementById('room_type').value;
-                        const roomCapacity = document.getElementById('room_capacity').value;
-                        const roomPrice = document.getElementById('room_price').value;
-                        const date = document.getElementById('date').value;
+            <script>
+                function createBooking() {
+                    const roomName = document.getElementById('room_name').value;
+                    const roomType = document.getElementById('room_type').value;
+                    const roomCapacity = document.getElementById('room_capacity').value;
+                    const roomPrice = document.getElementById('room_price').value;
+                    const date = document.getElementById('date').value;
 
-                        fetch('process_booking.php', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/x-www-form-urlencoded',
-                            },
-                            body: `room_name=${roomName}&room_type=${roomType}&room_capacity=${roomCapacity}&room_price=${roomPrice}&date=${date}`,
-                        })
-                            .then(response => response.text())
-                            .then(data => {
-                                alert(data);
-                                loadAvailableBookings();
-                            });
-                    }
-
-                    function loadAvailableBookings() {
-                        fetch('get_bookings.php')
-                            .then(response => response.json())
-                            .then(data => renderAvailableBookings(data));
-                    }
-
-                    function renderAvailableBookings(data) {
-                        const bookingsDiv = document.getElementById('available-bookings');
-                        bookingsDiv.innerHTML = '';
-                        data.forEach((booking, index) => {
-                            const bookingDiv = document.createElement('div');
-                            bookingDiv.className = 'booking-card'; // Assign the booking-card class
-                            bookingDiv.innerHTML = `
-                                <p>Room Name: ${booking.room_name}</p>
-                                <p>Room Type: ${booking.room_type}</p>
-                                <p>Room Capacity: ${booking.room_capacity}</p>
-                                <p>Room Price: ${booking.room_price}</p>
-                                <p>Date: ${booking.date}</p>
-                                <button onclick="editBooking(${index})">Edit</button>
-                                <button onclick="deleteBooking(${index})">Delete</button>
-                            `;
-                            bookingsDiv.appendChild(bookingDiv);
+                    fetch('process_booking.php', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                        },
+                        body: `room_name=${roomName}&room_type=${roomType}&room_capacity=${roomCapacity}&room_price=${roomPrice}&date=${date}`,
+                    })
+                        .then(response => response.text())
+                        .then(data => {
+                            alert(data);
+                            loadAvailableBookings();
                         });
-                    }
+                }
+
+                function loadAvailableBookings() {
+                    fetch('get_bookings.php')
+                        .then(response => response.json())
+                        .then(data => renderAvailableBookings(data));
+                }
+
+                function renderAvailableBookings(data) {
+                    const bookingsDiv = document.getElementById('available-bookings');
+                    bookingsDiv.innerHTML = '';
+                    data.forEach((booking, index) => {
+                        const bookingDiv = document.createElement('div');
+                        bookingDiv.className = 'booking-card'; // Assign the booking-card class
+                        bookingDiv.innerHTML = `
+                            <p>Room Name: ${booking.room_name}</p>
+                            <p>Room Type: ${booking.room_type}</p>
+                            <p>Room Capacity: ${booking.room_capacity}</p>
+                            <p>Room Price: ${booking.room_price}</p>
+                            <p>Date: ${booking.date}</p>
+                            <button onclick="editBooking(${index})">Edit</button>
+                            <button onclick="deleteBooking(${index})">Delete</button>
+                        `;
+                        bookingsDiv.appendChild(bookingDiv);
+                    });
+                }
 
 
-                    function editBooking(index) {
-                        const roomName = prompt("Enter new room name:");
-                        const roomType = prompt("Enter new room type:");
-                        const roomCapacity = prompt("Enter new room capacity:");
-                        const roomPrice = prompt("Enter new room price:");
-                        const date = prompt("Enter new date (YYYY-MM-DD):");
+                function editBooking(index) {
+                    const roomName = prompt("Enter new room name:");
+                    const roomType = prompt("Enter new room type:");
+                    const roomCapacity = prompt("Enter new room capacity:");
+                    const roomPrice = prompt("Enter new room price:");
+                    const date = prompt("Enter new date (YYYY-MM-DD):");
 
-                        if (roomName && roomType && roomCapacity && roomPrice && date) {
-                            fetch('edit_booking.php', {
-                                method: 'POST',
-                                headers: {
-                                    'Content-Type': 'application/x-www-form-urlencoded',
-                                },
-                                body: `index=${index}&room_name=${roomName}&room_type=${roomType}&room_capacity=${roomCapacity}&room_price=${roomPrice}&date=${date}`,
-                            })
-                                .then(response => response.text())
-                                .then(data => {
-                                    alert(data);
-                                    loadAvailableBookings();
-                                });
-                        }
-                    }
-
-                    function deleteBooking(index) {
-                        fetch('delete_booking.php', {
+                    if (roomName && roomType && roomCapacity && roomPrice && date) {
+                        fetch('edit_booking.php', {
                             method: 'POST',
                             headers: {
                                 'Content-Type': 'application/x-www-form-urlencoded',
                             },
-                            body: `index=${index}`,
+                            body: `index=${index}&room_name=${roomName}&room_type=${roomType}&room_capacity=${roomCapacity}&room_price=${roomPrice}&date=${date}`,
                         })
                             .then(response => response.text())
                             .then(data => {
@@ -137,12 +122,27 @@
                                 loadAvailableBookings();
                             });
                     }
+                }
 
-                    // Initial load of available bookings
-                    loadAvailableBookings();
+                function deleteBooking(index) {
+                    fetch('delete_booking.php', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                        },
+                        body: `index=${index}`,
+                    })
+                        .then(response => response.text())
+                        .then(data => {
+                            alert(data);
+                            loadAvailableBookings();
+                        });
+                }
 
-                </script>
-            </div>
+                // Initial load of available bookings
+                loadAvailableBookings();
+
+            </script>
         </div>
     </body>
 </html>
